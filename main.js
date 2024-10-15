e91e12628026e8d3f46a269084d65d1693240a47
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-10-15 07:09:39 +0530

    Made changes to the request URL

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 async function callWeather (location) {
     try {
-        console.log(location);
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=`;
+        const measurementUnit = document.querySelector('input[type="radio"]').value;
         const TOKEN = 'C47ASJQFUY3JSJWTSHV8VT6BT';
-        const response = await fetch(url + TOKEN, {mode: 'cors'});
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${measurementUnit}&key=${TOKEN}&contentType=json`;
+        const response = await fetch(url, {mode: 'cors'});
         if (!response.ok) throw new Error(`Response status: ${response.status}`);
         const data = await response.json();
         console.log(data);
+        // processWeatherData(data);
     }
     catch (err) {
         console.log(err);
     }
 }
 
-const buttonElement = document.querySelector('button');
+const buttonElement = document.querySelector('#search');
 buttonElement.addEventListener('click', (event) => {
     callWeather(document.querySelector('input').value);
     document.querySelector('form').reset();
     event.preventDefault();
 })
 
+function processWeatherData (jsonData) {
+    
 
-// callWeather('Chennai');
-// callWeather('Kashmir');
-// callWeather('koiulnmbubyv');
+}
