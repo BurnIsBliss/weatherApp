45687a022e1a2370454960d9207a62aa48ea6c90
BurnIsBliss <62287852+BurnIsBliss@users.noreply.github.com>
2024-10-18 05:52:50 +0530

    Finished setting up the GIPHY API to fetch the gifs for background image.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 async function callWeather (location) {
     try {
-        const measurementUnit = document.querySelector('input[type="radio"]').value;
+        const measurementUnit = document.querySelector('input[type="radio"]:checked').value;
         const TOKEN = 'C47ASJQFUY3JSJWTSHV8VT6BT';
         const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${measurementUnit}&key=${TOKEN}&contentType=json`;
         const response = await fetch(url, {mode: 'cors'});
         if (!response.ok) throw new Error(`Response status: ${response.status}`);
         const data = await response.json();
         console.log(data);
-        // processWeatherData(data);
+        processWeatherData(data, measurementUnit);
     }
     catch (err) {
         console.log(err);
@@ -17,11 +17,29 @@ async function callWeather (location) {
 const buttonElement = document.querySelector('#search');
 buttonElement.addEventListener('click', (event) => {
     callWeather(document.querySelector('input').value);
-    document.querySelector('form').reset();
+    document.querySelector('form > .searchContainer > input').value = '';
     event.preventDefault();
 })
 
-function processWeatherData (jsonData) {
-    
+function processWeatherData (jsonData, tempUnit) {
+    const location = jsonData.resolvedAddress;
+    const temp = jsonData.currentConditions.temp;
+    const temperatureUnit = (tempUnit == 'us')? '°F' : '°C';
+    console.log(`${location}: ${temp}${temperatureUnit}`);
+    fetchGIF(jsonData.currentConditions.conditions);
+}
 
+async function fetchGIF (backgroundImage) {
+    const gifURL = 'https://api.giphy.com/v1/gifs/translate?api_key=QFqig70RPPDEwGSYDSr9JG56tGomqwC3&s=' + backgroundImage;
+    const bodyElement = document.querySelector('body');
+    const response =  await fetch(gifURL, {mode: "cors"});
+    const dataJSON = await response.json();
+    console.log(dataJSON);
+    console.log(dataJSON.data.images.original.url);
+    try {
+        bodyElement.setAttribute('style', `background-image: url(${dataJSON.data.images.original.url})`)
+    }
+    catch (err) {
+        console.log(err.message);
+    }
 }
